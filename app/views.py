318677230bece6a101b73ed714c119b303b93f6b
Cavel Peck <cavelpeck@gmail.com>
2022-02-25 12:27:42 -0500

    your commit message

--- app/views.py
+++ app/views.py
@@ -4,10 +4,13 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
+from fileinput import filename
 import os
 from app import app
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from werkzeug.utils import secure_filename
+
+from .forms import UploadForm
 
 
 ###
@@ -28,6 +31,7 @@ def about():
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
+    form = UploadForm()
     if not session.get('logged_in'):
         abort(401)
 
@@ -36,11 +40,33 @@ def upload():
     # Validate file upload on submit
     if request.method == 'POST':
         # Get file data and save to your uploads folder
+        if form.validate_on_submit():
+            upload = form.upload.data
+            filename = secure_filename(upload.filename)
+            upload.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File Saved', 'success')
+            return redirect(url_for('home'))
+        else:
+             flash_errors('formobject')        
+    return render_template('upload.html', form=form)
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home'))
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    rootdir = os.getcwd()
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
 
-    return render_template('upload.html')
+@app.route('/files')
+def files():
+    return render_template('files.html', images= get_uploaded_images())
+
+def get_uploaded_images():
+    rootdir = os.getcwd()
+    imglst= []
+    for subdir, dirs, files in os.walk(rootdir + '/uploads'):
+        for file in files:
+            imglst.append(file)
+    return imglst[1:]        
+# print(get_uploaded_images())        
 
 
 @app.route('/login', methods=['POST', 'GET'])
